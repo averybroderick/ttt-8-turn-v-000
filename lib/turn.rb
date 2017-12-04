eba8dcd7de0320edb97ed4471a8ae7ed960a628f
Avery Broderick <avery.c.broderick@gmail.com>
2017-12-04 20:52:10 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,7 +14,7 @@ def position_taken?(board, index)
   !(board[index] == " " || board[index] == "" || board[index] == nil)
 end
 
-def valid_move?(index)
+def valid_move?(board, index)
   index.between?(0,8) && !(position_taken?(index))
 end
 
